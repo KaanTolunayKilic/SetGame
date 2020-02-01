8cdd65722029513fa78cff97278cab764c94832b
Kaan Tolunay Kilic <kaan-tolunay@hotmail.de>
2020-02-01 17:07:23 +0100

    content: add card and attributes

---
+++ SetGame/Model/Card.swift
@@ -0,0 +1,62 @@
+//
+//  Card.swift
+//  SetGame
+//
+//  Created by Kaan Tolunay Kilic on 01.02.20.
+//  Copyright © 2020 Kaan Tolunay Kilic. All rights reserved.
+//
+
+import Foundation
+
+struct Card: Equatable {
+    let color: Color
+    let shape: Shape
+    let fill: Fill
+    let amount: Amount
+    
+    static func == (lhs: Self, rhs: Self) -> Bool {
+        return (
+            lhs.color == rhs.color &&
+            lhs.shape == rhs.shape &&
+            lhs.fill == rhs.fill &&
+            lhs.amount == rhs.amount
+        )
+    }
+    
+    // MARK: card attributes
+    
+    enum Color: CardAttribute {
+        case first, second, third
+        
+        var all: [Card.Color] {
+            return [.first, .second, .third]
+        }
+    }
+    
+    enum Shape: CardAttribute {
+        case first, second, third
+        
+        var all: [Card.Shape] {
+            return [.first, .second, .third]
+        }
+    }
+    
+    enum Fill: CardAttribute {
+        case first, second, third
+        
+        var all: [Card.Fill] {
+            return [.first, .second, .third]
+        }
+    }
+    
+    enum Amount: CardAttribute {
+        case first, second, third
+        
+        var all: [Card.Amount] {
+            return [.first, .second, .third]
+        }
+    }
+}
+
+
+
---
+++ SetGame/Model/CardAttribute.swift
@@ -0,0 +1,13 @@
+//
+//  CardAttribute.swift
+//  SetGame
+//
+//  Created by Kaan Tolunay Kilic on 01.02.20.
+//  Copyright © 2020 Kaan Tolunay Kilic. All rights reserved.
+//
+
+import Foundation
+
+protocol CardAttribute {
+    var all: [Self] { get }
+}
